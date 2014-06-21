af3c48562e5a8fc60a268de0cb01d6de2b960d75
Flyingmana <flyingmana@googlemail.com>
2014-06-21 23:49:03 +0200

    add orderBy PackageName to package list

--- web/app.js
+++ web/app.js
@@ -2,6 +2,8 @@
 var composerWebuiApp = angular.module('composerWebuiApp', []);
 
 composerWebuiApp.controller('OverviewCtrl', function ($scope, $http) {
+    $scope.packagesOrder = 'prettyName';
+    
     $http.get('api/').success(function(data) {
         $scope.project  = { "name":data.name};
         $scope.packages = data.packages;
