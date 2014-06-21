d4bf0031dda1894ee8b2ba8d5e6c2ab04c2ec802
Flyingmana <flyingmana@googlemail.com>
2014-06-21 21:40:32 +0200

    make IO object availabil via DIC
    prepare for make switching possible,
    as users maybe want to get composers output

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 
 use Composer\Factory;
 use Composer\IO\NullIO;
+use Composer\IO\BufferIO;
 use Rabus\Composer\WebUI\Controller\MainController;
 use Silex\Application;
 use Silex\Provider\ServiceControllerServiceProvider;
@@ -11,11 +12,19 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $app = new Application;
 $app->register(new ServiceControllerServiceProvider());
 
+$app['io'] = function()
+{
+    $io = new NullIO;
+//    $io = new BufferIO;
+    return $io;
+};
+
+
 // Services
-$app['composer'] = function()
+$app['composer'] = function($app)
 {
     $factory = new Factory;
-    return $factory->create(new NullIO);
+    return $factory->create($app['io']);
 };
 
 $app['controllers.main'] = function ($app)
